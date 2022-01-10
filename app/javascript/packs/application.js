cd44d4fddae38d1cd9219cd88bcc7cd4a30a0ae0
Riz Michael Sim <rizsim214@gmail.com>
2022-01-10 22:39:13 +0800

    Generated Users Controller, Sessions Controller & Created SignIn/SignUp Layouts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,11 @@
-import "bootstrap"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
+import "bootstrap"
+import jquery from "jquery"
+window.$ = window.jquery = jquery
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,12 @@
-
-var webpack = require('webpack');
-
 const { environment } = require('@rails/webpacker')
+var webpack = require('webpack');
 
 environment.plugins.prepend('Provide',
 	new webpack.ProvidePlugin({
 		$: 'jquery/src/jquery.js',
 		jQuery: 'jquery/src/jquery.js',
 		Popper: ['popper.js', 'default']
-	}));
+	})
+);
 
 module.exports = environment
