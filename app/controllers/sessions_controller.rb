52f127ac7fd81c612dd721cf93c2a633ef0d6a51
Riz Michael Sim <rizsim214@gmail.com>
2022-01-12 23:22:30 +0800

    Login Actions and Users Action Updated to create and login to account with layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+      @user = User.find_by(email: params[:session][:email])
+      if @user && @user.authenticate(params[:session][:password])
+        login(@user)
+        flash[:success] = "Successfully logged in!"
+        redirect_to user_url(@user)
+      else
+        flash.now[:danger] = "Your credentials are incorrect!"
+        render "new"
+      end
+  end
+
+  
+
+  def destroy
+    logout
+    flash[:success] = "Successfully logged Out!"
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    @user = User.new
   end
 
-  def create
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url 
+    else
+      render "new"
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name , :email, :password, :password_confirmation)
   end
 end
